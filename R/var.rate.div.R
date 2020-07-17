3fe39a5e77eddb0ac46026cc9d79cf5489e49e6f
brpetrucci <bruno.petrucci@hotmail.com>
2020-07-16 19:56:34 -0500

    Changed some tests to don't run on var.rate.div since it is an unimportant function and they were taking long.

--- R/var.rate.div.R
+++ R/var.rate.div.R
@@ -45,184 +45,190 @@
 #' @examples
 #'
 #' # let us first create a vector of times to use in these examples.
-#'t <- seq(0, 50, 0.1)
-#'
-#'###
-#'# we can start simple: create a constant rate
-#'ff <- 0.1
-#'
-#'# set this up so we see rates next to diversity
-#'par(mfrow = c(1,2))
-#'
-#'# see how the rate looks
-#'r <- make.rate(0.5)
-#'plot(t, rep(r, length(t)), type = 'l')
-#'
-#'# get the diversity and plot it
-#'div <- var.rate.div(ff, t = t)
-#'plot(t, div, type = 'l')
-#'
-#'###
-#'# something a bit more complex: a linear rate
-#'ff <- function(t) {
-#'  return(0.01*t)
-#'}
-#'
-#'# visualize the rate
-#'r <- make.rate(ff)
-#'plot(t, r(t), type = 'l')
-#'
-#'# get the diversity and plot it
-#'div <- var.rate.div(ff, t = t)
-#'plot(t, div, type = 'l')
-#'
-#'###
-#'# remember: ff is diversity!
-#'
-#'# we can create speciation...
-#'pp <- function(t) {
-#'  return(-0.01*t + 0.2)
-#'}
-#'
-#'# ...and extinction...
-#'qq <- function(t) {
-#'  return(0.01*t)
-#'}
-#'
-#'# ...and code ff as diversification
-#'ff <- function(t) {
-#'  return(pp(t) - qq(t))
-#'}
-#'
-#'# visualize the rate
-#'r <- make.rate(ff)
-#'plot(t, r(t), type = 'l')
-#'
-#'# get diversity and plot it
-#'div <- var.rate.div(ff, n0 = 2, t)
-#'plot(t, div, type = 'l')
-#'
-#'###
-#'# remember: ff can be any time-varying function!
-#'
-#'# such as a sine
-#'ff <- function(t) {
-#'  return(sin(t)*0.5)
-#'}
-#'
-#'# visualize the rate
-#'r <- make.rate(ff)
-#'plot(t, r(t), type = 'l')
-#'
-#'# we can have any number of starting species
-#'div <- var.rate.div(ff, n0 = 2, t)
-#'plot(t, div, type = 'l')
-#'
-#'###
-#'# we can use ifelse() to make a step function like this
-#'ff <- function(t) {
-#'  return(ifelse(t < 2, 0.1,
-#'                ifelse(t < 3, 0.3,
-#'                       ifelse(t < 5, -0.2, 0.05))))
-#'}
-#'
-#'# change t so things are faster
-#'t <- seq(0, 10, 0.1)
-#'
-#'# visualize the rate
-#'r <- make.rate(ff)
-#'plot(t, r(t), type = 'l')
-#'
-#'# get the diversity and plot it
-#'div <- var.rate.div(ff, t = t)
-#'plot(t, div, type = 'l')
-#'
-#'# important note: this method of creating a step function might be annoying,
-#'# but when running thousands of simulations it will provide a much faster
-#'# integration than when using our method of transforming a rates and shifts
-#'# vector into a function of time
-#'
-#'###
-#'# ...which we can do as follows
-#'
-#'# rates vector
-#'ff <- c(0.1, 0.3, -0.2, 0.05)
-#'
-#'# rate shifts vector
-#'fShifts <- c(0, 2, 3, 5)
-#'
-#'# visualize the rate
-#'r <- make.rate(ff, tMax = 10, fShifts = fShifts)
-#'plot(t, r(t),type = 'l')
-#'
-#'# get the diversity and plot it
-#'div <- var.rate.div(ff, t = t, tMax = 10, fShifts = fShifts)
-#'plot(t, div, type = 'l')
-#'
-#'# note the delay in running var.rate.div using this method. integrating a step
-#'# function created using the methods in make.rate() is slow, as explained in
-#'# the make.rate documentation)
-#'
-#'# it is also impractical to supply a rate and a shifts vector and
-#'# have an environmental dependency, so in cases where one looks to run
-#'# more than a couple dozen simulations, and when one is looking to have a
-#'# step function modified by an environmental variable, consider using ifelse()
-#'
-#'# finally let us see what we can do with environmental variables
-#'
-#'# RPANDA supplies us with some really useful environmental dataframes
-#'# to use as an example, let us try temperature
-#'if (requireNamespace("RPANDA", quietly = TRUE)) {
-#'  # get the temperature data
-#'  data(InfTemp, package = "RPANDA")
-#'  
-#'  # diversification
-#'  ff <- function(t, env) {
-#'    return(0.002*env)
-#'  }
-#'  
-#'  # visualize the rate
-#'  r <- make.rate(ff, envF = InfTemp)
-#'  plot(t, r(t), type = 'l')
-#'  
-#'  # get diversity and plot it
-#'  div <- var.rate.div(ff, t = t, envF = InfTemp)
-#'  plot(t, div, type = 'l')
-#'  
-#'  ###
-#'  # we can also have a function that depends on both time AND temperature
-#'  
-#'  # diversification
-#'  ff <- function(t, env) {
-#'    return(0.03 * env - 0.01 * t)
-#'  }
-#'  
-#'  # visualize the rate
-#'  r <- make.rate(ff, envF = InfTemp)
-#'  plot(t, r(t), type = 'l')
-#'  
-#'  # get diversity and plot it
-#'  div <- var.rate.div(ff, t = t, envF = InfTemp)
-#'  plot(t, div, type = 'l')
-#'  
-#'  ###
-#'  # as mentioned above, we could also use ifelse() to construct a step function
-#'  # that is modulated by temperature
-#'  
-#'  # diversification
-#'  ff <- function(t, env) {
-#'    return(ifelse(t < 2, 0.1 + 0.01*env,
-#'                  ifelse(t < 5, 0.2 - 0.005*env,
-#'                         ifelse(t < 8, 0.1 + 0.005*env, 0))))
-#'  }
-#'  
-#'  # visualize the rate
-#'  r <- make.rate(ff, envF = InfTemp)
-#'  plot(t, r(t), type = 'l')
-#'  
-#'  div <- var.rate.div(ff, t = t, envF = InfTemp)
-#'  plot(t, div, type = 'l')
-#'}
+#' t <- seq(0, 50, 0.1)
+#' 
+#' ###
+#' # we can start simple: create a constant rate
+#' ff <- 0.1
+#' 
+#' # set this up so we see rates next to diversity
+#' par(mfrow = c(1,2))
+#' 
+#' # see how the rate looks
+#' r <- make.rate(0.5)
+#' plot(t, rep(r, length(t)), type = 'l')
+#' 
+#' # get the diversity and plot it
+#' div <- var.rate.div(ff, t = t)
+#' plot(t, div, type = 'l')
+#' 
+#' ###
+#' # something a bit more complex: a linear rate
+#' ff <- function(t) {
+#'   return(0.01*t)
+#' }
+#' 
+#' # visualize the rate
+#' r <- make.rate(ff)
+#' plot(t, r(t), type = 'l')
+#' 
+#' # get the diversity and plot it
+#' div <- var.rate.div(ff, t = t)
+#' plot(t, div, type = 'l')
+#' 
+#' ###
+#' # remember: ff is diversity!
+#' 
+#' # we can create speciation...
+#' pp <- function(t) {
+#'   return(-0.01*t + 0.2)
+#' }
+#' 
+#' # ...and extinction...
+#' qq <- function(t) {
+#'   return(0.01*t)
+#' }
+#' 
+#' # ...and code ff as diversification
+#' ff <- function(t) {
+#'   return(pp(t) - qq(t))
+#' }
+#' 
+#' # visualize the rate
+#' r <- make.rate(ff)
+#' plot(t, r(t), type = 'l')
+#' 
+#' # get diversity and plot it
+#' div <- var.rate.div(ff, n0 = 2, t)
+#' plot(t, div, type = 'l')
+#' 
+#' ###
+#' # remember: ff can be any time-varying function!
+#' 
+#' # such as a sine
+#' ff <- function(t) {
+#'   return(sin(t)*0.5)
+#' }
+#' 
+#' # visualize the rate
+#' r <- make.rate(ff)
+#' plot(t, r(t), type = 'l')
+#' 
+#' # we can have any number of starting species
+#' div <- var.rate.div(ff, n0 = 2, t)
+#' plot(t, div, type = 'l')
+#' 
+#' ###
+#' # we can use ifelse() to make a step function like this
+#' ff <- function(t) {
+#'   return(ifelse(t < 2, 0.1,
+#'           ifelse(t < 3, 0.3,
+#'            ifelse(t < 5, -0.2, 0.05))))
+#' }
+#' 
+#' # change t so things are faster
+#' t <- seq(0, 10, 0.1)
+#' 
+#' # visualize the rate
+#' r <- make.rate(ff)
+#' plot(t, r(t), type = 'l')
+#' 
+#' # get the diversity and plot it
+#' div <- var.rate.div(ff, t = t)
+#' plot(t, div, type = 'l')
+#' 
+#' # important note: this method of creating a step function might be annoying,
+#' # but when running thousands of simulations it will provide a much faster
+#' # integration than when using our method of transforming a rates and shifts
+#' # vector into a function of time
+#' 
+#' ###
+#' # ...which we can do as follows
+#' 
+#' # rates vector
+#' ff <- c(0.1, 0.3, -0.2, 0.05)
+#' 
+#' # rate shifts vector
+#' fShifts <- c(0, 2, 3, 5)
+#' 
+#' # visualize the rate
+#' r <- make.rate(ff, tMax = 10, fShifts = fShifts)
+#' plot(t, r(t),type = 'l')
+#' 
+#' # we set the longest running examples to not run in a package check, but a user
+#' # interested in the usability of the function may feel free to run them
+#' \dontrun{
+#'   # get the diversity and plot it
+#'   div <- var.rate.div(ff, t = t, tMax = 10, fShifts = fShifts)
+#'   plot(t, div, type = 'l')
+#' }
+#' 
+#' # note the delay in running var.rate.div using this method. integrating a step
+#' # function created using the methods in make.rate() is slow, as explained in
+#' # the make.rate documentation)
+#' 
+#' # it is also impractical to supply a rate and a shifts vector and
+#' # have an environmental dependency, so in cases where one looks to run
+#' # more than a couple dozen simulations, and when one is looking to have a
+#' # step function modified by an environmental variable, consider using ifelse()
+#' 
+#' # finally let us see what we can do with environmental variables
+#' 
+#' # RPANDA supplies us with some really useful environmental dataframes
+#' # to use as an example, let us try temperature
+#' if (requireNamespace("RPANDA", quietly = TRUE)) {
+#'   # get the temperature data
+#'   data(InfTemp, package = "RPANDA")
+#'   
+#'   # diversification
+#'   ff <- function(t, env) {
+#'     return(0.002*env)
+#'   }
+#'   
+#'   # visualize the rate
+#'   r <- make.rate(ff, envF = InfTemp)
+#'   plot(t, r(t), type = 'l')
+#'   
+#'   # get diversity and plot it
+#'   div <- var.rate.div(ff, t = t, envF = InfTemp)
+#'   plot(t, div, type = 'l')
+#'   
+#'   ###
+#'   # we can also have a function that depends on both time AND temperature
+#'   
+#'   # diversification
+#'   ff <- function(t, env) {
+#'     return(0.02 * env - 0.001 * t)
+#'   }
+#'   
+#'   # visualize the rate
+#'   r <- make.rate(ff, envF = InfTemp)
+#'   plot(t, r(t), type = 'l')
+#'   
+#'   \dontrun{
+#'     # get diversity and plot it
+#'     div <- var.rate.div(ff, t = t, envF = InfTemp)
+#'     plot(t, div, type = 'l')
+#' } 
+#'   ###
+#'   # as mentioned above, we could also use ifelse() to construct a step function
+#'   # that is modulated by temperature
+#'   
+#'   # diversification
+#'   ff <- function(t, env) {
+#'     return(ifelse(t < 2, 0.1 + 0.01*env,
+#'             ifelse(t < 5, 0.2 - 0.005*env,
+#'              ifelse(t < 8, 0.1 + 0.005*env, 0.08))))
+#'   }
+#'   
+#'   # visualize the rate
+#'   r <- make.rate(ff, envF = InfTemp)
+#'   plot(t, r(t), type = 'l')
+#' \dontrun{
+#'     div <- var.rate.div(ff, t = t, envF = InfTemp)
+#'     plot(t, div, type = 'l')
+#' }
+#' }
 #' 
 #' @name var.rate.div
 #' @rdname var.rate.div
--- cran-comments.md
+++ cran-comments.md
@@ -1,5 +1,6 @@
 ## Test environments
 * local Windows 10.0.19041, R 3.5.3
+* local macOS High Sierra 10.13.2, R 3.5.1
 * check_win (devel and release)
 * ubuntu xenian 18.04 (on travis-ci; devel and release)
 
@@ -9,7 +10,7 @@ There were no ERRORs, or WARNINGs.
 This is the first submission of this package, so there may be a first submission NOTE.
 
 Some example sections run over 10s to be able to showcase the flexibility of the package's functions. 
-The total example time was between 27s (Travis-CI devel) and 120s (win_builder release). This generated a NOTE.
+The total example time was between 27s (Travis-CI devel) and 90s (win_builder release). This generated a NOTE.
 
 There were no other NOTEs.
 
